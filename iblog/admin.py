0a518247859014b1f68cd5db4a8cb62a690432d5
avadh <rakholiyabalo@gmail.com>
2023-12-03 02:19:14 +0530

    changes in admin panel

--- iblog/admin.py
+++ iblog/admin.py
@@ -1,6 +1,24 @@
 from django.contrib import admin
 from .models import *
+
+
 # Register your models here.
 
-admin.site.register(Category)
-admin.site.register(Post)
+# configure of catagory means we want to show specific data at admin pannel
+class CatagoryAdmin(admin.ModelAdmin):
+    list_display = ('img_tag', 'title', 'desc', 'url', 'add_date', 'update_date')
+    # add serch field
+    search_fields = ('title', 'add_date')
+
+
+# pass custome catogoryadmin clss also at time of register bcz when register occure that time it give prefrence this model Catagory is also main model
+admin.site.register(Category, CatagoryAdmin)
+
+
+class postAdmin(admin.ModelAdmin):
+    list_display = ('title',)
+    search_fields = ('title',)
+    # filter for filter data
+    list_filter = ('Category',)
+    list_per_page = 5
+admin.site.register(Post, postAdmin)
--- iblog/models.py
+++ iblog/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.utils.html import format_html
+
 
 # Create your models here.
 
@@ -11,6 +13,13 @@ class Category(models.Model):
     add_date=models.DateTimeField(auto_now_add=True)
     update_date=models.DateTimeField(auto_now=True)
 
+    # for show image on admin pannel
+    def img_tag(self):
+        return format_html('<img src="/media/{}" style="width:40px;height:40px;border-radius:50%;" / >'.format(self.img))
+
+    # this function call when obkect is print we overwrite the function
+    def __str__(self):
+        return self.title
 
 class Post(models.Model):
     post_id=models.AutoField(primary_key=True)
@@ -18,5 +27,7 @@ class Post(models.Model):
     content=models.TextField()
     url=models.CharField(max_length=100)
     img=models.ImageField(upload_to='post/')
-    caatagory=models.ForeignKey(Category,on_delete=models.CASCADE)
+    Category=models.ForeignKey(Category,on_delete=models.CASCADE)
 
+    def __str__(self):
+        return self.title
